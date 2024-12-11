2e14fef8d6cca932b31dcdd8fcbbf811e5927906
eileen <zwleileen@gmail.com>
2024-12-11 14:01:36 +0800

    feat:completed q8

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // console.log("gruezi");
 // Exercise 1
-const maxOfTwoNumbers = (x, y) => {
+function maxOfTwoNumbers(x, y) {
   if (x >= y) {
     return x;
   } else {
     return y;
   }
-};
+}
 console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
 
 // Exercise 2
@@ -27,6 +27,49 @@ function isCharAVowel(m) {
     return "false";
   }
 }
-console.log("Exercise 3 Result:", isCharAVowel("p"));
+console.log("Exercise 3 Result:", isCharAVowel("a"));
 
 // Exercise 4
+function generateEmail(name, domain) {
+  return name + "@" + domain;
+}
+console.log("Exercise 4 Result:", generateEmail("johnsmith", "example.com"));
+
+// Exercise 5
+function greetUser(name, time) {
+  return "Good " + time + ", " + name + "!";
+}
+console.log("Exercise 5 Result:", greetUser("Sam", "morning"));
+
+// Exercise 6
+function maxOfThree(x, y, z) {
+  if (x >= y && x >= z) {
+    return x;
+  } else if (y >= x && y >= z) {
+    return y;
+  } else {
+    return z;
+  }
+}
+console.log("Exercise 6 Result:", maxOfThree(5, 10, 8));
+
+// Exercise 7
+function calculateTip(bill, tipPercentage) {
+  const tip = bill * (tipPercentage / 100);
+  return tip;
+}
+console.log("Exercise 7 Result:", calculateTip(50, 20));
+
+// Exercise 8
+function convertTemperature(temperature, scale) {
+  if (scale === "C") {
+    const convertToF = (temperature * 9) / 5 + 32;
+    return convertToF + " (Fahrenheit)";
+  } else {
+    const convertToC = (temperature - 32) * (5 / 9);
+    return convertToC + " (Celsius)";
+  }
+}
+console.log("Exercise 8 Result:", convertTemperature(32, "F"));
+
+// Exercise 9
