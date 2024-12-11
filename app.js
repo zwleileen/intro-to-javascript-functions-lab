3aad72a928f81b34b89c15fa25008a3187f1fd86
eileen <zwleileen@gmail.com>
2024-12-11 13:04:05 +0800

    feat:completed q3

--- app.js
+++ app.js
@@ -20,3 +20,13 @@ function isAdult(age) {
 console.log("Exercise 2 Result:", isAdult(21));
 
 // Exercise 3
+function isCharAVowel(m) {
+  if (m === "a" || m === "e" || m === "i" || m === "o" || m === "u") {
+    return "true";
+  } else {
+    return "false";
+  }
+}
+console.log("Exercise 3 Result:", isCharAVowel("p"));
+
+// Exercise 4
