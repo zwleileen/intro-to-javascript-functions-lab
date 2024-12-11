2fe849a64fa311f6362b443417ece5c1b2d83517
eileen <zwleileen@gmail.com>
2024-12-11 12:15:58 +0800

    feat:completed q2

---
+++ app.js
@@ -0,0 +1,22 @@
+// console.log("gruezi");
+// Exercise 1
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+};
+console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
+
+// Exercise 2
+function isAdult(age) {
+  if (age >= 18) {
+    return "Adult";
+  } else {
+    return "Minor";
+  }
+}
+console.log("Exercise 2 Result:", isAdult(21));
+
+// Exercise 3
